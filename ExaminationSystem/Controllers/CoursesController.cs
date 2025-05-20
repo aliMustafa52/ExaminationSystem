e836633f6198e6ff3411f24dce306c61a99cc2b4
aliMustafa52 <104281551+aliMustafa52@users.noreply.github.com>
2025-05-21 01:35:21 +0300

    Add CoursesController

--- ExaminationSystem/Controllers/CoursesController.cs
+++ ExaminationSystem/Controllers/CoursesController.cs
@@ -1,6 +1,7 @@
 ﻿using ExaminationSystem.Data;
-using Microsoft.AspNetCore.Http;
+using ExaminationSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExaminationSystem.Controllers
 {
@@ -11,9 +12,46 @@ namespace ExaminationSystem.Controllers
         private readonly AppDbContext _context = context;
 
         [HttpGet("")]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Ok(_context.Courses.ToList());
+            var courses = await _context.Courses
+                            .AsNoTracking()
+                            .ToListAsync();
+
+            return Ok(courses);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var course = await _context.Courses
+                            .AsNoTracking()
+                            .FirstOrDefaultAsync(x => x.Id == id);
+
+            return Ok(course);
+        }
+
+        [HttpPost("")]
+        public async Task<bool> AddCourse(Course course)
+        {
+            await _context.Courses.AddAsync(course);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<bool> DeleteCourse(int id)
+        {
+            var course = await _context.Courses
+                            .FirstOrDefaultAsync(x => x.Id == id);
+            if(course is null)
+                return false;
+
+            _context.Remove(course);
+            await _context.SaveChangesAsync();
+
+            return true;
         }
     }
 }
